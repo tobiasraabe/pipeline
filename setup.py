20fd7749785f469f11318bb437f75dca5f007ec9
Tobias Raabe <tobiasraabe@uni-bonn.de>
2020-03-29 23:18:19 +0200

    Release 0.0.2:

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name="pipeline",
-    version="0.0.1",
+    version="0.0.2",
     author="Tobias Raabe",
     author_email="raabe@posteo.de",
     python_requires=">=3.6.0",
