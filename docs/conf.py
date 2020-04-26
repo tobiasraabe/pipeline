c6306ae11d1c1ede2690695a759a142373b2de19
Tobias Raabe <raabe@posteo.de>
2020-04-26 11:06:13 +0200

    New documentation style.

--- docs/conf.py
+++ docs/conf.py
@@ -33,7 +33,6 @@ extensions = [
     "numpydoc",
     "sphinx.ext.extlinks",
     "sphinx.ext.intersphinx",
-    "sphinx_rtd_theme",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -65,7 +64,7 @@ intersphinx_mapping = {
 # The theme to use for HTML and HTML Help pages.  See the documentation for a list of
 # builtin themes.
 
-html_theme = "sphinx_rtd_theme"
+html_theme = "pydata_sphinx_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here, relative
 # to this directory. They are copied after the builtin static files, so a file named
